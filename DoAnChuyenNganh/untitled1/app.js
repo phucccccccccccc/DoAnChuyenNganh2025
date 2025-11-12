3a9a34e4250a304bb6952f978f12be3768151880
Phucccccccccccc <phucdanghaihoang@gmail.com>
2025-11-12 16:42:53 +0700

    cap nhat

--- DoAnChuyenNganh/untitled1/app.js
+++ DoAnChuyenNganh/untitled1/app.js
@@ -31,6 +31,7 @@ app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- DoAnChuyenNganh/untitled1/routes/admin.js
+++ DoAnChuyenNganh/untitled1/routes/admin.js
@@ -9,15 +9,22 @@ var router = express.Router();
         res.render('admin/login', {layout: 'admin/login'});
     });
 router.get('/', (req, res) => {
-    res.render('layouts/admin', { layout: 'layouts/admin', title: 'Bảng điều khiển' });
+    res.render('admin/index', { layout: 'layouts/admin', title: 'Bảng điều khiển' });
 });
 router.get('/register', function (req, res) {
-    res.render('admin/register', {layout: 'admin/register'});
+    res.render('admin/register', {layout:    'admin/register'});
 });
 router.get('/forgot-password', function (req, res) {
     res.render('admin/forgot-password', {layout: 'admin/forgot-password'});
 });
+router.get('/category', (req, res) => {
+    res.render('admin/category/category-list', { layout: 'layouts/admin', title: 'Quản lý danh mục' });
+});
 
+// Trang product
+router.get('/product', (req, res) => {
+    res.render('admin/product/product-list', { layout: 'layouts/admin', title: 'Quản lý sản phẩm' });
+});
 
     module.exports = router;
 
