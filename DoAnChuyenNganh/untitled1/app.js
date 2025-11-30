e6e8375fbf07c5f30b4adbfa636390db21bbf00c
Phucccccccccccc <phucdanghaihoang@gmail.com>
2025-11-30 22:30:18 +0700

    Chuc nang dang nhap and dang ky

--- DoAnChuyenNganh/untitled1/app.js
+++ DoAnChuyenNganh/untitled1/app.js
@@ -30,12 +30,73 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 
+const mongoose = require('mongoose');
+const User = require('./models/User'); // đường dẫn tới model User của bạn
+const bcryptjs = require('bcrypt'); // bcryptjs hoặc bcrypt
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect('mongodb://127.0.0.1:27017/node')
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("Error connected to MongoDB",err));
+
+
+// app.js
+app.post('/register', async (req, res) => {
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+    // Kiểm tra password khớp
+    if(password !== confirmPassword){
+        return res.send("Passwords do not match!");
+    }
+
+    // Kiểm tra email đã tồn tại chưa
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        return res.send("Email already exists!");
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword
+    });
+
+    await newUser.save();
+
+    res.send(`
+        <script>
+            alert('Đăng ký thành công!');
+            window.location.href = '/admin/login';
+        </script>
+    `);
+});
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }).then((user) => {
+        if (user) {
+            bcryptjs.compare(req.body.password, user.password, (err, matched) => {
+                if (err) return res.status(500).send("Server error");
+
+                if (matched) {
+                    // Login thành công → redirect sang /admin
+                    res.redirect('/admin');
+                } else {
+                    res.send("Sai mật khẩu");
+                }
+            });
+        } else {
+            res.send("Email không tồn tại");
+        }
+    }).catch(err => res.status(500).send("Server error"));
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
---
+++ DoAnChuyenNganh/untitled1/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 5,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    }
+});
+//const User = mongoose.model('User', userSchema);
+//module.exports = User;
+module.exports = mongoose.model('users', UserSchema);
